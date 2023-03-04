977f273928d6bdda5ec509043b1a5056f25bf661
Katie Lonsdale <katie952@gmail.com>
2023-03-03 20:47:09 -0700

    fix: tests for most accurate and least accurate team to include season argument

--- spec/season_statistics_spec.rb
+++ spec/season_statistics_spec.rb
@@ -36,14 +36,16 @@ RSpec.describe SeasonStatistics do
 
   describe '#most_accurate_team' do
     it 'returns the team with the best ratio of shots to goals' do
-      expect(@season_stats.most_accurate_team).to eq('Utah Royals FC')
+      expect(@season_stats.most_accurate_team('20122013')).to eq('FC Dallas')
+      expect(@season_stats.most_accurate_team('20142015')).to eq('Utah Royals FC')
     end
   end
 
   describe '#least_accurate_team' do
     it 'returns the team with the worst ratio of shots to goals' do
-      expect(@season_stats.least_accurate_team).to eq('Columbus Crew SC')
-      # getting a tie...not sure how to best adjust method
+      expect(@season_stats.least_accurate_team('20122013')).to eq('Houston Dynamo')
+      expect(@season_stats.least_accurate_team('20142015')).to eq('Columbus Crew SC')
+      # not sure what to do in event of tie
     end
   end
 
