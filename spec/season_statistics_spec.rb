0ec07ef24307222bbdea4c64e4b3cf3ca0f74469
Katie Lonsdale <katie952@gmail.com>
2023-03-03 18:28:06 -0700

    test: add accuracy test

--- spec/season_statistics_spec.rb
+++ spec/season_statistics_spec.rb
@@ -2,9 +2,9 @@ require './spec/spec_helper'
 
 RSpec.describe SeasonStatistics do
   before(:each) do
-    games = './data/games.csv'
+    games = './data/mock_games.csv'
     teams = './data/teams.csv'
-    game_teams = './data/game_teams.csv'
+    game_teams = './data/mock_game_teams.csv'
     @locations = {
       games: games,
       teams: teams,
@@ -31,6 +31,19 @@ RSpec.describe SeasonStatistics do
     it 'has game_teams' do
       expect(@season_stats.game_teams).to be_a Array
       expect(@season_stats.game_teams.sample).to be_a GameTeams
+    end
+  end
+
+  describe '#most_accurate_team' do
+    xit 'returns the team with the best ratio of shots to goals' do
+      expect(@season_stats.most_accurate_team).to eq('Utah Royals FC')
+    end
+  end
+
+  describe '#accuracy' do
+    it 'returns goals divided by number of shots' do
+      expect(@season_stats.accuracy(5,10)).to eq 0.50
+      expect(@season_stats.accuracy(3, 9)).to eq 0.33
     end
   end
 end
