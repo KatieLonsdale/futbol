61a8ce853e7ec367c5149ee85a21936cc9858b86
Nick Sacco <nicksacco10@gmail.com>
2023-03-03 18:56:47 -0800

    Feat: best_offense and helpers

--- lib/league_statistics.rb
+++ lib/league_statistics.rb
@@ -8,22 +8,41 @@ class LeagueStatistics < Stats
   def count_of_teams
     @teams.count
   end
-#Name of the team with the highest average number of goals scored per game across all seasons.
+
   def best_offense
     best_offense = Hash.new(0)
     @game_teams.each do |game_team|
-      average_number_of_goals = (game_team.goals).fdiv(count_of_teams)
-      best_offense[game_team.team_id] += average_number_of_goals
+      average_number_of_goals = (total_goals(game_team.team_id)).fdiv(total_games_by_team(game_team.team_id))
+      best_offense[game_team.team_id] = average_number_of_goals
     end
-    best_offense.values
-    require 'pry'; binding.pry
+    sorted = best_offense.sort_by { |_,v| -v }
+    team_id_string = sorted[0][0]
+    team_id_converter(team_id_string)
   end
-# create hash - keys are team id
-# values are average goals scored per game
-# sort values 
-# return highest value
-# convert highest value's key to actual team name.
- # return team name
+
+  def total_goals(team_id)
+    total_goals = 0
+    @game_teams.each do |game_team|
+      total_goals += game_team.goals if game_team.team_id == team_id
+    end
+    total_goals
+  end
+  
+  def total_games_by_team(team_id)
+    total_games = 0
+    @game_teams.each do |game_team|
+      total_games += 1 if game_team.team_id == team_id
+    end
+    total_games
+  end
+
+  def team_id_converter(team_id_string)
+    best_team = " "
+    @teams.each do |team|
+      best_team = team.team_name if team.team_id == team_id_string
+    end
+    best_team
+  end
 
   # def worst_offense
   # end
