08a029b9332a7c53e935ef1682d3a6d11e3e0bb9
Katie Lonsdale <katie952@gmail.com>
2023-03-03 19:04:15 -0700

    feat: add goals and shots by team method

--- lib/season_statistics.rb
+++ lib/season_statistics.rb
@@ -6,7 +6,6 @@ class SeasonStatistics < Stats
   end
 
   # def most_accurate_team
-  #   require 'pry'; binding.pry
   #   # group by team id values shots goals
   #   # shots for all games = game.shots
   #   # goals for all games = game.goals
@@ -15,8 +14,24 @@ class SeasonStatistics < Stats
   #   # return most accurate
 
   #   games_by_team = @game_teams.group_by {|game_team| game_team.team_id}
-  #     end
+  #   games_by_team = goals_and_shots_by_team(games_by_team)
+  #   games_by_team = games_by_team.transform_values! do |array| 
+  #     accuracy(array[0], array[1])
+  #   end
+  #   games_by_team.max_by{|team, accuracy| accuracy}
+  #   require 'pry'; binding.pry
   # end
+
+  def goals_and_shots_by_team(hash)
+    hash.transform_values do |games|
+      [goals = 0, shots = 0]
+      games.each do |game|
+        shots += game.shots.to_i
+        goals += game.goals.to_i
+      end
+      [goals, shots]
+    end
+  end
 
   def accuracy(goals, shots)
     goals.fdiv(shots).round(2)
