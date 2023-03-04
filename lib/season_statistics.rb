b0d59ddf2cda7303fe07c98d73e54d49ddd13d64
Katie Lonsdale <katie952@gmail.com>
2023-03-03 18:28:28 -0700

    feat: add accuracy method

--- lib/season_statistics.rb
+++ lib/season_statistics.rb
@@ -4,4 +4,21 @@ class SeasonStatistics < Stats
   def initialize(locations)
     super
   end
+
+  # def most_accurate_team
+  #   require 'pry'; binding.pry
+  #   # group by team id values shots goals
+  #   # shots for all games = game.shots
+  #   # goals for all games = game.goals
+  #   # get ratio of shots to goals (accuracy helper method)
+  #   # sort by accuracy
+  #   # return most accurate
+
+  #   games_by_team = @game_teams.group_by {|game_team| game_team.team_id}
+  #     end
+  # end
+
+  def accuracy(goals, shots)
+    goals.fdiv(shots).round(2)
+  end
 end
