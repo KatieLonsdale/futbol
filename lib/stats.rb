fb0c1a46837e102f21f0491fc7cdb678fb3160a5
Katie Lonsdale <katie952@gmail.com>
2023-03-05 14:00:50 -0700

    refactor: clean up code

--- lib/stats.rb
+++ lib/stats.rb
@@ -1,14 +1,16 @@
 require 'csv'
-require_relative 'team'
-require_relative 'game'
-require_relative 'game_teams'
-# require_relative 'stat_tracker'
+
 class Stats
-  attr_reader :teams, :games, :game_teams
+  attr_reader :teams, 
+              :games, 
+              :game_teams
   
   def initialize(locations)
-    @teams = CSV.read(locations[:teams], headers: true, header_converters: :symbol).map { |row| Team.new(row) }
-    @games = CSV.read(locations[:games], headers: true, header_converters: :symbol).map { |row| Game.new(row) }
-    @game_teams = CSV.read(locations[:game_teams], headers: true, header_converters: :symbol).map { |row| GameTeams.new(row) }
+    @teams = CSV.read(locations[:teams], headers: true, 
+      header_converters: :symbol).map { |row| Team.new(row) }
+    @games = CSV.read(locations[:games], headers: true, 
+      header_converters: :symbol).map { |row| Game.new(row) }
+    @game_teams = CSV.read(locations[:game_teams], headers: true, 
+      header_converters: :symbol).map { |row| GameTeams.new(row) }
   end
 end
