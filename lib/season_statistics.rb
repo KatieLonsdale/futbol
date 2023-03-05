3397dcb7e335f924527bc1a8f6ef696a79c03457
Nick Sacco <nicksacco10@gmail.com>
2023-03-04 18:23:59 -0800

    Feat: worst_coach method

--- lib/season_statistics.rb
+++ lib/season_statistics.rb
@@ -49,10 +49,13 @@ class SeasonStatistics < Stats
     games_by_coach = win_percentage(games_by_coach)
     games_by_coach.max_by{|coach, win_percentage| win_percentage}.first
   end
-  #number of games played in a season by a coach(helper method?)
-  #number of games won
-  #average
-  #return the highest average coach name ""
+
+  def worst_coach(season)
+    games_in_season = filter_game_teams_by_season(season)
+    games_by_coach = games_in_season.group_by{|game_team| game_team.head_coach}
+    games_by_coach = win_percentage(games_by_coach)
+    games_by_coach.min_by{|coach, win_percentage| win_percentage}.first
+  end
 
   def win_percentage(games_by_coach)
     games_by_coach.transform_values do |games|
